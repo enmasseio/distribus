170e410b4168facc47437ec7d78822b463b35017
jos <wjosdejong@gmail.com>
2014-06-18 10:52:06 +0200

    Implemented notify to send notifications without response

--- test/requestify.test.js
+++ test/requestify.test.js
@@ -7,7 +7,7 @@ var assert = require('assert'),
 
 describe('requestify', function() {
 
-  it ('should turn a socket in a request/response channel', function (done) {
+  it ('should send a request via a socket and receive a response', function (done) {
     var server, client;
 
     // get a free port
@@ -76,6 +76,63 @@ describe('requestify', function() {
         .then(done);
   });
 
+  it ('should send a notification via a socket (no response)', function (done) {
+    var server, client;
+
+
+    // get a free port
+    new Promise(function (resolve, reject) {
+      freeport(function (err, port) {
+        err ? reject(err) : resolve(port);
+      })
+    })
+
+        // open a server
+        .then(function (port) {
+          return new Promise(function (resolve, reject) {
+            server = new ws.Server({port: port});
+
+            server.on('connection', function(client) {
+              client = requestify(client);
+
+              client.onrequest = function onrequest (message) {
+                assert.equal(message, 'This is a notification');
+
+                client.close();
+                server.close();
+
+                done();
+              };
+            });
+
+            resolve(port);
+          });
+        })
+
+        // open a client
+        .then(function (port) {
+          return new Promise(function (resolve, reject) {
+            client = requestify(new WebSocket('ws://localhost:' + port));
+
+            client.onopen = resolve;
+          });
+        })
+
+        // send a notification
+        .then(function () {
+          client.notify('This is a notification')
+              .then(function (response) {
+                assert.equal(response, null);
+                assert.ok(true, 'notification has been send');
+              })
+              .catch(function (err) {
+                assert.ok(false, 'should not fail');
+              });
+        })
+  });
+
   // TODO: test errors, broken connection, etc
+
+  // TODO: test notify
 
 });
